f92b51bd9db9f73e066b30ff7c1c754db9bcbc81
shivamshaw07 <shivamshaw9005@gmail.com>
2024-04-28 01:46:50 +0530

    home section 3 made responsive

--- frontend/src/components/Home/HomeSection6/BecomeAnInstructor.jsx
+++ frontend/src/components/Home/HomeSection6/BecomeAnInstructor.jsx
@@ -6,15 +6,15 @@ import { FaArrowRight } from 'react-icons/fa6';
 const BecomeAnInstructor = () => {
   return (
     <div className='w-full h-auto pt-[15vh] pb-16'>
-        <div className='w-[85%] mx-auto flex justify-center items-center'>
-            <div className='w-[50%] relative'>
-                <div className='w-[90%] h-full absolute z-10 bg-white bottom-5 -left-5'></div>
-                <img src={instructor} alt="instructor" className='w-[90%] h-full z-20 relative shadow-lg shadow-black'/>
+        <div className='w-[95%] md:w-[85%] lg:w-[85%] xl:w-[85%] mx-auto flex flex-col md:flex-row lg:flex-row xl:flex-row justify-center items-center gap-6'>
+            <div className='w-[100%] md:w-[50%] lg:w-[50%] xl:w-[50%]  relative flex justify-center items-center mx-auto'>
+                <div className='w-[100%] h-full absolute z-10 bg-white bottom-5 -left-5'></div>
+                <img src={instructor} alt="instructor" className='w-[100%] h-full z-20 relative shadow-lg shadow-black'/>
             </div>
-            <div className='w-[50%] flex flex-col justify-start items-start gap-7'>
-                <h1 className=' text-4xl text-white font-semibold  '>Become An <span className='text-[#cbab61]'>Instructor.</span></h1>
+            <div className='w-[100%] md:w-[50%] lg:w-[50%] xl:w-[50%]  flex flex-col justify-start items-start gap-7'>
+                <h1 className=' text-4xl text-white font-semibold w-full '>Become An <span className='text-[#cbab61]'>Instructor.</span></h1>
                 <p className='text-white'>Instructors from around the world teach millions of students on StudyNotion. We provide the tools and skills to teach what you love.</p>
-                <button className=' bg-glod-color hover:bg-[#b99b55] px-4 py-2 rounded text-white'><span className='mr-1 font-medium '>Start Teaching Today</span><span><FaArrowRight className='inline'/></span></button>
+                <button className='w-full md:w-auto lg:w-auto xl:w-auto bg-glod-color hover:bg-[#b99b55] px-4 py-2 rounded text-white'><span className='mr-1 font-medium '>Start Teaching Today</span><span><FaArrowRight className='inline'/></span></button>
 
             </div>
         </div>
