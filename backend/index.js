d110722884fd35a899a89c16604b1dde5465a6e0
shivamshaw07 <shivamshaw9005@gmail.com>
2024-05-25 16:56:42 +0530

    cors error solved

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import dbconnect from './config/database.js'
+import express from 'express';
+import dbconnect from './config/database.js';
 import { config as configDotenv } from 'dotenv';
 import authRoute from './routes/auth.js';
 import userRoute from './routes/userRoute.js';
@@ -8,34 +8,37 @@ import { cloudinaryset } from './config/cloudinary.js';
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
 import studentRoute from './routes/student.js';
-configDotenv()
-cloudinaryset()
 
-const port = process.env.PORT || 5000
+configDotenv();
+cloudinaryset();
+
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors(
-    {
-        origin:["https://code-gyaan.vercel.app/"],
-        methods:["POST","GET","PUT","DELETE"],
-        credentials: true
-    }
-));
+// CORS configuration
+app.use(cors({
+    origin: ["https://code-gyaan.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
-    useTempFiles:true,
-    tempFileDir:'/tmp/'
-}))
-app.use('/auth',authRoute);
-app.use('/user',userRoute);
-app.use('/instructor',instructorRoute);
-app.use('/student',studentRoute);
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}));
 
-app.use('/',(req,res)=>{
-    res.send("server is running")
-})
-app.listen(port,()=>{
-    console.log("server is running at port ",port);
+app.use('/auth', authRoute);
+app.use('/user', userRoute);
+app.use('/instructor', instructorRoute);
+app.use('/student', studentRoute);
+
+app.use('/', (req, res) => {
+    res.send("server is running");
+});
+
+app.listen(port, () => {
+    console.log("server is running at port", port);
     dbconnect();
-})
+});
