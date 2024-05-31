0c099d21fc8e7c69a3440a828601f4ce4bb3d456
shivamshaw07 <shivamshaw9005@gmail.com>
2024-05-31 14:06:23 +0530

    Bug fix

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -2,17 +2,17 @@ import { Router } from "express";
 import { auth, isStudent } from "../middlewares/auth.js";
 import { getEnrolledCourses } from "../controllers/course.js";
 import { updateCourseProgress } from "../controllers/courseProgress.js";
-import {createRating,getAverageRating,getAllRating} from '../controllers/ratingAndReview.js';
+// import {createRating,getAverageRating,getAllRating} from '../controllers/ratingAndReview.js';
 
 const studentRoute  = Router();
 
 studentRoute.get("/enrolledCourses/:id",auth,isStudent,getEnrolledCourses)
 studentRoute.post("/updateCourseProgress", auth, isStudent, updateCourseProgress);
 
-//rating and review
-studentRoute.post("/createRating", auth, isStudent, createRating)
-studentRoute.get("/getAverageRating", getAverageRating)
-studentRoute.get("/getReviews", getAllRating)
+// //rating and review
+// studentRoute.post("/createRating", auth, isStudent, createRating)
+// studentRoute.get("/getAverageRating", getAverageRating)
+// studentRoute.get("/getReviews", getAllRating)
 
 
 
