2fc577038b9824433f0535ea3dd95140b44af58d
SHIVAM KUMAR SHAW <108209696+shivamshaw07@users.noreply.github.com>
2024-06-27 20:57:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 // CORS configuration
 app.use(cors(
     {
-        origin:["http://localhost:5173","https://codegyaan.vercel.app"],
+        origin:["*"],
         methods:["POST","GET","PUT","DELETE"],
         credentials:true
     }
